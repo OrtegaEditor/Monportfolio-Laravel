f210a46d0027ba52f8dad26a6c1e065788eb2c38
ortegaeditor <ortegaeditor@gmail.com>
2026-02-21 22:58:35 +0200

    Formulaire de contact

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route de contact publique (PAS de middleware auth !)
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -27,35 +30,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
 });
 
 require __DIR__.'/auth.php';
-
-// git add .
-// git commit -m "Description claire de la modification"
-// git push
-
-
-// Database
-    // USERNAME : if0_41203655
-    // MySQL Password : Okd4V7AZM3hu
-    // MySQL Database Name : if0_41203655_XXX
-    // MySQL Hostname  : sql306.infinityfree.com
-    // MySQL Port (optional) : 3306
-    // Database Name	: if0_41203655_portfolio
-
-
-
-// TP Details for if0_41203655
-    // FTP Username  : if0_41203655
-    // FTP Password   : Okd4V7AZM3hu
-    // FTP Hostname    : ftpupload.net
-
-//  Accounts details
-    //Username :   if0_41203655
-    // Password :  Okd4V7AZM3hu
-    // Label   : Website for marcelinfosso.wuaze.com
-    // Website IP  : 185.27.134.202
-    // Home Directory  : /home/vol3_3/infinityfree.com/if0_41203655
